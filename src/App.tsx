644e2fb992a2f9a248275ac1f37dc5eb69924075
Oleh Marynchuk <happyhikaro@gmail.com>
2022-11-07 14:03:50 +0200

    change router

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,18 @@
 import JobsList from './components/JobsList';
 import JobDetailsPage  from './components/jobDetailsPage';
-import { useState, Suspense } from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { useState } from 'react';
+import { Routes, Route, HashRouter } from 'react-router-dom'
 
 const App: React.FunctionComponent = () => {
   const [currentJob, setCurrentJob] = useState({})
 
   return (
-    <BrowserRouter>
-      <Suspense fallback={<h1 className='loading_title'>Loading...</h1>}>
+    <HashRouter>
         <Routes>
           <Route path="/" element={<JobsList setCurrentJob={setCurrentJob}/>} />
           <Route path="detailed" element={<JobDetailsPage currentJob={currentJob}/>} />
         </Routes>
-      </Suspense>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
